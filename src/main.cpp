a3d7e613821e7e8742becba68b37181db3f77e41
Max Kaufmann <max.kaufmann@gmail.com>
2012-08-14 00:03:38 -0700

    simplified test

--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,28 @@
 #include <vector>
 #include <iostream>
 
-class Duck {
-public:
-    ID id;
-
-private:
+struct Duck {
     const char* msg;
-    
-public:
     void SetMessage(const char* aMsg) { msg = aMsg; }
     void Quack() { std::cout << msg << std::endl; }
 };
 
 class QuackSystem : public EntityComponentSystem::ISystem  {
 private:
-    CompactPool<Duck> mPool;
-    PoolIndex mIndex[32];
-    // PinnedPool<Duck> mPool;
-    // uint32_t mMask[1];
+    PinnedPool<Duck> mPool;
+    uint32_t mMask[1];
     Duck mDucks[32];
 
 public:
-    QuackSystem() : mPool(32, mIndex, mDucks) {}
-    // QuackSystem() : mPool(32, mMask, mDucks) {}
+    QuackSystem() : mPool(32, mMask, mDucks) {}
 
     ID CreateComponent() { return mPool.TakeOut(); }
     void DestroyComponent(ID c) {  mPool.PutBack(c); }
+    
     Duck& GetDuck(ID c) { return mPool[c]; }
     
     void BatchQuack() {
-        for(auto p=mPool.Enumerate(); p.Next();) {
-            p->Quack();
-        }
+        for(auto p=mPool.Enumerate(); p.Next();) { p->Quack(); }
     }
     
 };
