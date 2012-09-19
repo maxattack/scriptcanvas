5631d5367ae7a9ac4ec408877030fd05630aec3f
Max Kaufmann <max.kaufmann@gmail.com>
2012-09-19 11:12:50 -0700

    simplified records -- only move ones that need moving

--- src/SceneSystem.cpp
+++ src/SceneSystem.cpp
@@ -10,26 +10,27 @@ namespace SceneSystem {
 //------------------------------------------------------
 
 struct NodeSlot {
-	// A lookup slot for accessing data records,
-	// which are reordered to preserve a compact buffer
+	// for thumbprint validation (in case slot is reused)
 	ID id;
-	uint16_t index;
-	uint16_t next;
-};
 
-struct ComponentData {
-	uint32_t mask;
-};
+	uint16_t poseIndex;
 
-struct TreeData {
+	// freelist linked list
+	uint16_t nextFreeSlot;
+
+	// tree data
 	ID parent;
 	ID firstChild;
 	ID nextSibling;
 	ID prevSibling;
+
+	// component data
+	uint32_t componentMask;
 };
 
 struct PoseData {
-	int parentIndex;
+	uint16_t parentIndex;
+	uint16_t slotIndex;
 	transform localToParent;
 };
 
@@ -41,9 +42,6 @@ static uint32_t sNodeCount;
 static uint16_t sNodeFreelistEnqueue;
 static uint16_t sNodeFreelistDequeue;
 static NodeSlot sNodeSlots[MAX_NODES];
-static uint16_t sNodeBackBuffer[MAX_NODES];
-static TreeData sNodeTrees[MAX_NODES];
-static ComponentData sNodeComps[MAX_NODES];
 static PoseData sNodePoses[MAX_NODES];
 static int mFirstInvalidDagIndex;
 static int mLastInvalidDagIndex;
@@ -55,22 +53,13 @@ static IManager* sComponentManagers[MAX_COMPONENT_TYPES];
 
 static NodeSlot& Slot(ID id) {
 	ASSERT((id&0xffff)<MAX_NODES);
+	ASSERT(NodeValid(id));
 	return sNodeSlots[id & 0xffff];
-}
-
-static TreeData& Heirarchy(ID id) {
-	ASSERT(NodeValid(id));
-	return sNodeTrees[sNodeSlots[id&0xffff].index];
-}
-
-static ComponentData& Component(ID id) {
-	ASSERT(NodeValid(id));
-	return sNodeComps[sNodeSlots[id&0xffff].index];
 }
 
 static PoseData& Pose(ID id) {
 	ASSERT(NodeValid(id));
-	return sNodePoses[sNodeSlots[id&0xffff].index];
+	return sNodePoses[sNodeSlots[id&0xffff].poseIndex];
 }
 
 //------------------------------------------------------
@@ -85,14 +74,14 @@ void Initialize() {
 	sNodeFreelistDequeue = 0;
 	for (unsigned i=0; i<MAX_NODES; ++i) {
 		sNodeSlots[i].id = i;
-		sNodeSlots[i].index = USHRT_MAX;
-		sNodeSlots[i].next = i+1;
+		sNodeSlots[i].poseIndex = USHRT_MAX;
+		sNodeSlots[i].nextFreeSlot = i+1;
 	}
 }
 
 bool NodeValid(ID id) {
 	NodeSlot& slot = sNodeSlots[id & 0xffff];
-	return slot.id == id && slot.index != USHRT_MAX;
+	return slot.id == id && slot.poseIndex != USHRT_MAX;
 }
 
 int NodeCount() {
@@ -104,16 +93,18 @@ ID CreateNode(ID parent) {
 	// Allocate a new node at the end of the buffer
 	// Dequeue a slot
 	auto& slot = sNodeSlots[sNodeFreelistDequeue];
-	sNodeFreelistDequeue = slot.next;
+	sNodeFreelistDequeue = slot.nextFreeSlot;
 	// Give the ID a unique thumbprint
 	slot.id = (0xffff & slot.id) + ((((0xffff0000&slot.id)>>16)%0xffff)<<16) + 0x10000;
 	// Allocate a new buffer location
-	slot.index = sNodeCount++;
+	slot.poseIndex = sNodeCount++;
 	// initialize records
-	sNodeBackBuffer[slot.index] = 0xffff & slot.id;
-	sNodePoses[slot.index] = { -1, Transform() };
-	sNodeComps[slot.index].mask = 0;
-	sNodeTrees[slot.index] = { 0, 0, 0, 0 };
+	sNodePoses[slot.poseIndex] = { USHRT_MAX, uint16_t(0xffff&slot.id), Transform() };
+	slot.componentMask = 0;
+	slot.parent = 0;
+	slot.firstChild = 0;
+	slot.nextSibling = 0;
+	slot.prevSibling = 0;
 	if (parent) {
 		AttachNode(parent, slot.id);
 	}
@@ -122,7 +113,7 @@ ID CreateNode(ID parent) {
 
 void AttachNode(ID parent, ID child) {
 	ASSERT(parent != child);
-	auto& childData = Heirarchy(child);
+	auto& childData = Slot(child);
 	// check existing parents
 	if (childData.parent) {
 		if (childData.parent == parent) {
@@ -133,13 +124,13 @@ void AttachNode(ID parent, ID child) {
 	}
 	// update records
 	childData.parent = parent;
-	auto& parentData = Heirarchy(parent);
+	auto& parentData = Slot(parent);
 	if (parentData.firstChild) {
 		childData.nextSibling = parentData.firstChild;
-		Heirarchy(parentData.firstChild).prevSibling = child;
+		Slot(parentData.firstChild).prevSibling = child;
 	}
-	int pIndex = Slot(parent).index;
-	int chIndex = Slot(child).index;
+	int pIndex = Slot(parent).poseIndex;
+	int chIndex = Slot(child).poseIndex;
 	parentData.firstChild = child;
 	Pose(child).parentIndex = pIndex;
 	// DAG is invalid if children are listed before parents
@@ -154,12 +145,12 @@ void AttachNode(ID parent, ID child) {
 }
 
 void DetachNode(ID child) {
-	auto& data = Heirarchy(child);
+	auto& data = Slot(child);
 	if (data.parent) {
-		auto& parent = Heirarchy(data.parent);
+		auto& parent = Slot(data.parent);
 		if (parent.firstChild == child) parent.firstChild = data.nextSibling;
-		if (data.nextSibling) Heirarchy(data.nextSibling).prevSibling = data.prevSibling;
-		if (data.prevSibling) Heirarchy(data.prevSibling).nextSibling = data.nextSibling;
+		if (data.nextSibling) Slot(data.nextSibling).prevSibling = data.prevSibling;
+		if (data.prevSibling) Slot(data.prevSibling).nextSibling = data.nextSibling;
 		data.parent = 0;
 		data.nextSibling = 0;
 		data.prevSibling = 0;
@@ -168,17 +159,17 @@ void DetachNode(ID child) {
 }
 
 ID Parent(ID node) {
-	return Heirarchy(node).parent;
+	return Slot(node).parent;
 }
 
 ChildIterator::ChildIterator(ID node) {
-	current = Heirarchy(node).firstChild;
+	current = Slot(node).firstChild;
 }
 
 bool ChildIterator::Next(ID *outNode) {
 	if (current) {
 		*outNode = current;
-		current = Heirarchy(current).nextSibling;
+		current = Slot(current).nextSibling;
 		return true;
 	} else {
 		return false;
@@ -186,7 +177,7 @@ bool ChildIterator::Next(ID *outNode) {
 }
 
 int Index(ID node) {
-	return Slot(node).index+1;
+	return Slot(node).poseIndex;
 }
 
 transform& LocalToParent(ID node) {
@@ -194,7 +185,7 @@ transform& LocalToParent(ID node) {
 }
 
 transform LocalToWorld(ID node) {
-	auto parent = Heirarchy(node).parent;
+	auto parent = Slot(node).parent;
 	return parent ? LocalToParent(node) * LocalToWorld(parent) : LocalToParent(node);
 }
 
@@ -215,7 +206,6 @@ void Update(RenderBuffer *vbuf) {
 		memset(scratchpadMarks+mFirstInvalidDagIndex, 0, mLastInvalidDagIndex-mFirstInvalidDagIndex+1);
 		int readPos = mFirstInvalidDagIndex;
 		int writePos = readPos;
-
 		do {
 			// parent(s) in the invalid-dag-slice, need to write it first
 			int parentIndex = sNodePoses[readPos].parentIndex;
@@ -248,13 +238,10 @@ void Update(RenderBuffer *vbuf) {
 		for(int i=mFirstInvalidDagIndex; i<=mLastInvalidDagIndex; ++i) {
 			if (scratchpad[i] != i) {
 				std::swap(
-					sNodeSlots[sNodeBackBuffer[i]].index, 
-					sNodeSlots[sNodeBackBuffer[scratchpad[i]]].index
+					sNodeSlots[sNodePoses[i].slotIndex].poseIndex, 
+					sNodeSlots[sNodePoses[scratchpad[i]].slotIndex].poseIndex
 				);
-				std::swap(sNodeBackBuffer[i], sNodeBackBuffer[scratchpad[i]]);
 				std::swap(sNodePoses[i], sNodePoses[scratchpad[i]]);
-				std::swap(sNodeTrees[i], sNodeTrees[scratchpad[i]]);
-				std::swap(sNodeComps[i], sNodeComps[scratchpad[i]]);
 				int j=i+1;
 				while(scratchpad[j] != i && j <= mLastInvalidDagIndex) {
 					++j;
@@ -265,8 +252,9 @@ void Update(RenderBuffer *vbuf) {
 			}
 		}
 		for(int i=mFirstInvalidDagIndex; i<=mLastInvalidDagIndex; ++i) {
-			if (sNodeTrees[i].parent) {
-				sNodePoses[i].parentIndex = Slot(sNodeTrees[i].parent).index;
+			ID parent = Slot(sNodePoses[i].slotIndex).parent;
+			if (parent) {
+				sNodePoses[i].parentIndex = Slot(parent).poseIndex;
 			}
 			
 		}
@@ -274,9 +262,12 @@ void Update(RenderBuffer *vbuf) {
 		mLastInvalidDagIndex = -1;
 	}
 	// fast-compute local-to-world buffer
-	vbuf->transforms[0] = Transform();
 	for(int i=0; i<sNodeCount; ++i) {
-		vbuf->transforms[i+1] = sNodePoses[i].localToParent * vbuf->transforms[sNodePoses[i].parentIndex+1];
+		if (sNodePoses[i].parentIndex == USHRT_MAX) {
+			vbuf->transforms[i] = sNodePoses[i].localToParent;
+		} else {
+			vbuf->transforms[i] = sNodePoses[i].localToParent * vbuf->transforms[sNodePoses[i].parentIndex];
+		}
 	}
 }
 
@@ -291,23 +282,23 @@ void RegisterComponentManager(ID componentType, IManager* pMgr) {
 void AddComponent(ID node, ID componentType) {
 	ASSERT(sComponentManagers[componentType]);
 	ASSERT(!HasComponent(node, componentType));
-	Component(node).mask |= (0x80000000 >> componentType);
+	Slot(node).componentMask |= (0x80000000 >> componentType);
 	sComponentManagers[componentType]->CreateComponent(node);
 }
 
 bool HasComponent(ID node, ID componentType) {
-	return Component(node).mask & (0x80000000 >> componentType);
+	return Slot(node).componentMask & (0x80000000 >> componentType);
 }
 
 void RemoveComponent(ID node, ID componentType) {
 	ASSERT(sComponentManagers[componentType]);
 	ASSERT(HasComponent(node, componentType));
 	sComponentManagers[componentType]->DestroyComponent(node);
-	Component(node).mask ^= (0x80000000 >> componentType);
+	Slot(node).componentMask ^= (0x80000000 >> componentType);
 }
 
 ComponentIterator::ComponentIterator(ID node) {
-	mask = Component(node).mask;
+	mask = Slot(node).componentMask;
 }
 
 bool ComponentIterator::Next(ID *outComponentType) {
@@ -325,7 +316,7 @@ void DestroyNode(ID node) {
 	ASSERT(NodeValid(node));
 	DetachNode(node);
 	// tear down children
-	auto& data = Heirarchy(node);
+	auto& data = Slot(node);
 	while(data.firstChild) {
 		DestroyNode(data.firstChild);
 	}
@@ -342,20 +333,15 @@ void DestroyNode(ID node) {
 	// relocate last element into this slot and deallocate the last slot
 	sNodeCount--;
 	if (sNodeCount) {
+
 		// move last element into this place
-		sNodeBackBuffer[slot.index] = sNodeBackBuffer[sNodeCount];
-		sNodeTrees[slot.index] = sNodeTrees[sNodeCount];
-		sNodeComps[slot.index] = sNodeComps[sNodeCount];
-		sNodePoses[slot.index] = sNodePoses[sNodeCount];
-		
-		// Update the slot of the last element
-		sNodeSlots[sNodeBackBuffer[sNodeCount]].index = slot.index;
+		sNodePoses[slot.poseIndex] = sNodePoses[sNodeCount];
+		sNodeSlots[sNodePoses[sNodeCount].slotIndex].poseIndex = slot.poseIndex;
 		
 		// check that DAG is still valid
-		auto& hdata = sNodeTrees[slot.index];
-		int chIndex = slot.index;
-		if (hdata.parent) {
-			int pIndex = Slot(hdata.parent).index;
+		int chIndex = slot.poseIndex;
+		if (slot.parent) {
+			int pIndex = Slot(slot.parent).poseIndex;
 			if (chIndex < pIndex) {
 				if (mFirstInvalidDagIndex == -1 || chIndex < mFirstInvalidDagIndex) {
 					mFirstInvalidDagIndex = chIndex;
@@ -365,14 +351,14 @@ void DestroyNode(ID node) {
 				}				
 			}
 		}
-		for(ID cid = hdata.firstChild; cid; cid=Heirarchy(cid).nextSibling) {
+		for(ID cid = slot.firstChild; cid; cid=Slot(cid).nextSibling) {
 			Pose(cid).parentIndex = chIndex;
 		}
 	}
 
 	// Invalidate the slot and enqueue in the freelist
-	slot.index = USHRT_MAX;
-	sNodeSlots[sNodeFreelistEnqueue].next = node & 0xffff;
+	slot.poseIndex = USHRT_MAX;
+	sNodeSlots[sNodeFreelistEnqueue].nextFreeSlot = node & 0xffff;
 	sNodeFreelistEnqueue = node & 0xffff;
 }
 
--- src/main.lua
+++ src/main.lua
@@ -11,30 +11,12 @@ function createPlanet(x, y, radius, r, g, b)
 	return result
 end
 
--- test
-local n1 = createNode()
-local n2 = createNode(n1)
-local n3 = createNode(n2)
-local n4 = createNode(n3)
-local n5 = createNode()
-attachTo(n5, n1)
-destroyNode(n5)
-
-n1 = createNode()
 local planetx = createPlanet(100, 100, 48, 0.33, 0.25, 0.65)
-n2 = createNode()
 local sun = createPlanet(400, 400, 32, 1, 1, 0.25)
-n3 = createNode()
 local urth = createPlanet(128, 0, 24, 0.5, 0.5, 0.8)
-n4 = createNode()
 local moon = createPlanet(48, 0, 12, 0.4, 0.4, 0.4)	
-n5 = createNode()
 attachTo(sun, urth)
 attachTo(urth, moon)
-
--- test
-attachTo(n5, n1)
-destroyNode(n5)
 
 local isDone = false
 
@@ -43,14 +25,10 @@ while not isDone do
 	local t = input.seconds()
 	local mx, my = input.mousePosition()
 	setGeometryRadius(geometry(sun), 32 + 4*sin(2.1*tau*t))
-	--print("setPosition sun")
 	setPosition(sun, mx, my)
-	--print("setRotation sun")
 	setRotation(sun, -0.11*360*t)
-	--print("setRotation urth")
 	setRotation(urth, 360*t)
 	local x,y = positionOf(planetx)
-	--print("setPosition planetX")
 	setPosition(planetx, 0.1*mx + 0.9*x, 0.1*my + 0.9*y)
 	paint()
 end
