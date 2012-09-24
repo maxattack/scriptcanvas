a7ba91836faeacf6655744fe6730321ea43e0e68
Max Kaufmann <max.kaufmann@gmail.com>
2012-09-24 14:02:55 -0700

    bindings oh yeah

--- src/main.lua
+++ src/main.lua
@@ -1,67 +1,68 @@
-local isDone = false
-while not isDone do
-	scene.paint()
+function import(ns)
+	for k,v in pairs(ns) do
+		_G[k] = v
+	end
 end
 
--- import(scene)
--- import(math)
+import(scene)
+import(math)
 
--- -- create the planets
+-- create the planets
 
--- function createPlanet(x, y, color, radius)
--- 	local result = createNode()
--- 	setPosition(result, x, y)
--- 	circle.add(result, color, radius)
--- 	return result
--- end
+function createPlanet(x, y, color, radius)
+	local result = createNode()
+	setPosition(result, vec(x, y))
+	circle.create(result, color, radius)
+	return result
+end
 
--- local planetx = createPlanet(100, 100, 0x444488, 48)
--- local sun = createPlanet(400, 400, 0xffff00, 32)
--- local urth = createPlanet(128, 0, 0x8888ff, 24)
--- local moon = createPlanet(48, 0, 0x888888, 12)	
--- attachTo(sun, urth)
--- attachTo(urth, moon)
+local planetx = createPlanet(100, 100, 0x444488, 48)
+local sun = createPlanet(400, 400, 0xffff00, 32)
+local urth = createPlanet(128, 0, 0x8888ff, 24)
+local moon = createPlanet(48, 0, 0x888888, 12)	
+attachTo(sun, urth)
+attachTo(urth, moon)
 
--- -- create the noodle
+-- create the noodle
 
--- function addNode(x, y)
--- 	local result = createNode()
--- 	setPosition(result, x, y)
--- 	return result
--- end
+function addNode(x, y)
+	local result = createNode()
+	setPosition(result, vec(x, y))
+	return result
+end
 
--- local p0 = addNode(0, -50)
--- local p1 = addNode(200, 100)
--- local p2 = addNode(200, 600)
--- local p3 = addNode(300, 850)
--- setDirection(p0, 50, 50)
--- setDirection(p3, 50, 50)
--- local noodle = spline.addMaterial(8, 0x777755)
--- spline.addSegment(p0, p1, noodle)
--- spline.addSegment(p1, p2, noodle);
--- spline.addSegment(p2, p3, noodle);
+local p0 = addNode(0, -50)
+local p1 = addNode(200, 100)
+local p2 = addNode(200, 600)
+local p3 = addNode(300, 850)
+setDirection(p0, vec(50, 50))
+setDirection(p3, vec(50, 50))
+local noodle = spline.addMaterial(8, 0x777755)
+spline.addSegment(p0, p1, noodle)
+spline.addSegment(p1, p2, noodle);
+spline.addSegment(p2, p3, noodle);
 
--- -- game loop
+-- game loop
 
--- local isDone = false
--- while not isDone do
--- 	local tau = 2 * 3.14156
--- 	local t = input.seconds()
--- 	local mx, my = input.mousePosition()
+local isDone = false
+while not isDone do
+	local tau = 2 * 3.14156
+	local t = input.seconds()
+	local mp = input.mousePosition()
 	
--- 	-- planets
--- 	circle.setRadius(sun, 32 + 4*sin(2.1*tau*t))
--- 	setPosition(sun, mx, my)
--- 	setRotation(sun, -0.11*360*t)
--- 	setRotation(urth, 360*t)
--- 	local x,y = position(planetx)
--- 	setPosition(planetx, 0.1*mx + 0.9*x, 0.1*my + 0.9*y)
+	-- planets
+	circle.setRadius(sun, 32 + 4*sin(2.1*tau*t))
+	setPosition(sun, mp)
+	setRotation(sun, -0.11*360*t)
+	setRotation(urth, 360*t)
+	local p = position(planetx)
+	setPosition(planetx, 0.1*mp + 0.9*p)
 
--- 	-- noodle dance
--- 	setDirection(p1, (mx - 200), (my - 100))
--- 	setDirection(p2, (200 - mx), (600 - my))
--- 	spline.setWeight(noodle, 8  + 32 * (0.5 + 0.5 * sin(tau*t)))
+	-- noodle dance
+	setDirection(p1, mp - vec(200, 100))
+	setDirection(p2, vec(200,600) - mp)
+	spline.setWeight(noodle, 8  + 32 * (0.5 + 0.5 * sin(tau*t)))
 
--- 	paint()
--- end
+	paint()
+end
 
--- tools/write_bindings.py
+++ tools/write_bindings.py
@@ -1,15 +1,13 @@
-import os
-import os.path
-import sys
+import os, os.path, sys, traceback
 
-_MODULES = {
+MODULES = {
 
 	'scene': {
 		'paint': 			'void SceneSystem::Paint()',
 		'nodeCount': 		'int SceneSystem::NodeCount()',
 		'createNode': 		'ID SceneSystem::CreateNode(ID parent=0)',
 		'attachTo': 		'void SceneSystem::AttachNode(ID parent, ID child)',
-		'detach': 			'void SceneSystem::Detach(ID child)',
+		'detach': 			'void SceneSystem::DetachNode(ID child)',
 		'parent': 			'ID SceneSystem::Parent(ID node)',
 		'destroyNode':		'void SceneSystem::DestroyNode(ID node)',
 		'position': 		'float2 SceneSystem::Position(ID node)',
@@ -45,10 +43,11 @@ _MODULES = {
 
 }
 
-MODULES = {
+_MODULES = {
 	'scene': {
-		'paint':	'void SceneSystem::Paint()'		
-	}
+		'paint':			'void SceneSystem::Paint()',
+		'nodeCount': 		'int SceneSystem::NodeCount()',
+	},
 }
 
 HEADER = '''#include "ScriptSystem.h"
@@ -58,7 +57,10 @@ HEADER = '''#include "ScriptSystem.h"
 namespace ScriptSystem {
 namespace Internal {
 
-static void LoadModule(lua_State* L, const char* name, const luaL_Reg *p) {
+static void LoadModule(lua_State* L, const char* name, const luaL_Reg *p, int nfuncs) {
+	// I think this is a dup of a luaauxlib function, but I figure,
+	// don't fix it if it ain't broke!
+	lua_createtable(L, 0, nfuncs);
 	lua_newtable(L);
 	while(p->name) {
 		lua_pushstring(L, p->name);
@@ -67,6 +69,10 @@ static void LoadModule(lua_State* L, const char* name, const luaL_Reg *p) {
 		++p;
 	}
 	lua_setglobal(L, name);
+}
+
+inline float2 Float2(lua_Vector v) {
+	return *(float2*)(&v);
 }
 
 '''
@@ -84,37 +90,86 @@ def main():
 			src.write("};\n\n")
 		src.write("}\n\n")
 		src.write('void Bind(lua_State* L) {\n')
-		for module in MODULES:
-			#write_line(src, 'luaL_newlib(L, %s);' % module)
-			write_line(src, 'Internal::LoadModule(L, "%s", Internal::_%s);' % (module, module))
+		for module, signatures in MODULES.iteritems():
+			write_line(src, 'Internal::LoadModule(L, "%s", Internal::_%s, %d);' % (module, module, len(signatures)))
 		src.write('}\n\n}\n\n')
 
 
 def write_function_implementation(src, module, name, sig):
 	result_type, command, args = unpack_signature(sig)
 	src.write('static int _%s_%s(lua_State* L) {\n' % (module, name))
-	# gather arguments (if DEBUG check types)
+	# collect args
+	write_line(src, "int nargs = lua_gettop(L);")
 	if args:
-		fail("TODO: gather args from lua stack")
+		total_arg_count = len(args)
+		opt_arg_count = len([def_val for _,_,def_val in args if def_val is not None])
+		req_arg_count = total_arg_count - opt_arg_count
+		if opt_arg_count:
+			write_line(src, "ASSERT(%d <= nargs && nargs <= %d);" % (req_arg_count, total_arg_count))
+		else:
+			write_line(src, "ASSERT(nargs == %d);" % total_arg_count)
+
+		def required_arg_str(stack_index, type):
+			if type in ('int'):
+				return 'luaL_checkinteger(L, %d)' % stack_index
+			elif type in ('ID', 'uint32_t'):
+				return 'luaL_checkunsigned(L, %d)' % stack_index
+			elif type in ('float', 'double'):
+				return 'luaL_checknumber(L, %d)' % stack_index
+			elif type in ('float2'):
+				return 'Float2(luaL_checkvec(L, %d))' % stack_index
+			elif type in ('color_t'):
+				return 'RGB(luaL_checkunsigned(L, %d))' % stack_index
+			else:
+				dofail()
+
+		def opt_arg_str(stack_index, type, def_value):
+			if type in ('int'):
+				return 'luaL_optinteger(L, %d, %s)' % (stack_index, def_value)
+			elif type in ('ID', 'uint32_t'):
+				return 'luaL_optunsigned(L, %d, %s)' % (stack_index, def_value)
+			elif type in ('float', 'double'):
+				return 'luaL_optnumber(L, %d, %s)' % (stack_index, def_value)
+			elif type in ('float2'):
+				return 'Float2(luaL_optvec(L, %d, %s))' % (stack_index, def_value)
+			elif type in ('color_t'):
+				return 'RGB(luaL_optunsigned(L, %d, %s))' % (stack_index, def_value)
+			else:
+				dofail()
+
+		for i, (arg_type, arg_name, arg_def_value) in enumerate(args):
+			write_line(src, "%s arg_%s = %s;" % (
+				arg_type, arg_name, 
+				required_arg_str(i+1, arg_type) if i < req_arg_count else opt_arg_str(i+1, arg_type, arg_def_value)
+			))
+
 	else:
-		# TODO: validate no args
-		pass
-	# alloc result
-	if result_type != 'void':
-		fail("TODO: alloc return")
+		write_line(src, "ASSERT(nargs == 0);")
 	# invoke function
 	src.write("\t")
-	src.write(command)
-	src.write("(")
-	# pass arguments
-	if args:
-		fail("TODO: pass args")
-	src.write(");\n")
+	if result_type != 'void':
+		src.write("%s result = " % result_type)
+	src.write("%s(%s);\n" % (command, ','.join("arg_"+arg_name for _,arg_name,_ in args)))
 	# push results
 	num_results = 0
-	if args:
-		fail("TODO: push args on lua stack")
-	src.write("\treturn %d;\n"  % num_results)
+	if result_type in ('int'):
+		num_results = 1
+		write_line(src, "lua_pushinteger(L, result);")
+	elif result_type in ('uint32_t', 'ID'):
+		num_results = 1
+		write_line(src, "lua_pushunsigned(L, result);")
+	elif result_type in ('float', 'double'):
+		num_results = 1
+		write_line(src, "lua_pushnumber(L, result);")
+	elif result_type in ("float2"):
+		num_results = 1
+		write_line(src, "lua_pushvec(L, result.x, result.y);")
+	elif result_type in ("color_t"):
+		num_results = 1
+		write_line(src, "lua_pushunsigned(L, result.RGB());")
+	elif result_type !='void':
+		fail("Unknown return type: %s" % result_type)
+	write_line(src, "return %d;"  % num_results)
 	src.write("}\n\n")
 
 def write_line(src, line):
@@ -125,30 +180,51 @@ def write_line(src, line):
 
 def unpack_signature(sig):
 	# parse the signature string
-	# TODO: check validity?
+	def dofail():
+		fail("Bad Signature: %s" % sig)
 	first_space = sig.find(' ')
 	first_paren = sig.find('(')
 	last_paren = sig.rfind(')')
+	if first_space == -1 or first_paren == -1 or last_paren == -1:
+		dofail()
+	if first_space >= first_paren or first_paren >= last_paren:
+		dofail()
 	return_value = sig[0:first_space]
 	name = sig[first_space+1:first_paren]
 	arg_sig = sig[first_paren+1:last_paren].strip()
+	if not return_value or not name:
+		dofail()
 	tokens = ( arg.strip() for arg in arg_sig.split(',') if arg.strip() )
 	def unpack_arg_token(sig):
 		first_space = sig.find(' ')
+		if first_space == -1:
+			dofail()
 		type = sig[0:first_space].strip()
 		name = sig[first_space+1:].strip()
+		if not type or not name:
+			dofail()
 		if '=' in name:
 			idx = name.find('=')
 			actual_name = name[0:idx].strip()
 			default_value = name[idx+1:].strip()
+			if not actual_name or not default_value:
+				dofail()
 			return (type,actual_name,default_value)
 		else:
 			return (type,name,None)
 	args = tuple( unpack_arg_token(token) for token in tokens )
+	# validate that opt args are on the end
+	seen_opt_arg = False
+	for _,_,def_val in args:
+		if def_val is not None:
+			seen_opt_arg = True
+		elif seen_opt_arg:
+			dofail()
 	return (return_value, name, args)
 
 def fail(msg):
 	print msg
+	traceback.print_stack()
 	exit(-1)
 
 if __name__ == '__main__':
